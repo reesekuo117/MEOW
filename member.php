d5c8ebae9d4ae9647ae41a8e72bfa4962afc04da
reesekuo117 <toivo.017@gmail.com>
2022-09-02 17:16:35 +0800

    add

---
+++ member.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__. '/parts/meow_db.php';  // /開頭
+$pageName ='home'; //頁面名稱
+?>
+
+<?php include __DIR__. '/parts/html-head.php'; ?>
+<?php include __DIR__. '/parts/navbar.php'; ?>
+<div class="container">
+    <h2>
+        Hello
+    </h2>
+</div>
+<?php include __DIR__. '/parts/scripts.php'; ?>
+<?php include __DIR__. '/parts/html-foot.php'; ?>
