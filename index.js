779e4794260f588f7748f7e03adf8b4997336f97
Bajohn Huang <jimmybaga@hotmail.com>
2022-09-04 13:45:01 +0800

    加月老&喵對話 下滑 大黃圈圈

--- index.js
+++ index.js
@@ -10,5 +10,22 @@ const typeWriter = selector => {
             setTimeout(() => _type(i + 1), 100)
         })()
 }
+// setTimeout(typeWriter(".js-type-writer"), 5000)
 
 typeWriter(".js-type-writer")
+
+const typeWriter2 = selector => {
+    const el = document.querySelector(selector)
+    const text = el.innerHTML
+
+        ; (function _type(i = 0) {
+            if (i === text.length) return
+
+            el.innerHTML =
+                text.substring(0, i + 1) + '<span aria-hidden="true"></span>'
+            setTimeout(() => _type(i + 1), 100)
+        })()
+}
+
+setTimeout(typeWriter2(".js-type-writer2"), 1000)
+// FIXME:
