8d8dfca7798db6819e42be838b34ed72f2b745b3
“Libby <hay1025@hotmail.com>
2022-09-05 08:56:17 +0800

    all

--- culture.php
+++ culture.php
@@ -1,12 +1,12 @@
 <?php
 require __DIR__. '/parts/meow_db.php';  // /開頭
-$pageName ='home'; //頁面名稱
+$pageName =''; //頁面名稱
 ?>
 
 <?php include __DIR__. '/parts/html-head.php'; ?>
 <?php include __DIR__. '/parts/navbar.php'; ?>
-<div class="container">
-   
+<div class="container-fulid">
+    <img src="/imgs/culture-main.jpg" alt="">
 </div>
 <?php include __DIR__. '/parts/scripts.php'; ?>
 <?php include __DIR__. '/parts/html-foot.php'; ?>
