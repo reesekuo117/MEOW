e41490cfd9799353bc08efce799e643ef9bed887
Bajohn Huang <jimmybaga@hotmail.com>
2022-09-11 13:52:31 +0800

    貓 hover 好

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
-const typeWriter = selector => {
-    const el = document.querySelector(selector)
-    const text = el.innerHTML
+// const typeWriter = selector => {
+//     const el = document.querySelector(selector)
+//     const text = el.innerHTML
 
-        ; (function _type(i = 0) {
-            if (i === text.length) return
+//         ; (function _type(i = 0) {
+//             if (i === text.length) return
 
-            el.innerHTML =
-                text.substring(0, i + 1) + '<span aria-hidden="true"></span>'
-            setTimeout(() => _type(i + 1), 100)
-        })()
-}
-// setTimeout(typeWriter(".js-type-writer"), 5000)
+//             el.innerHTML =
+//                 text.substring(0, i + 1) + '<span aria-hidden="true"></span>'
+//             setTimeout(() => _type(i + 1), 100)
+//         })()
+// }
+// // setTimeout(typeWriter(".js-type-writer"), 5000)
 
-typeWriter(".js-type-writer")
+// typeWriter(".js-type-writer")
 
-const typeWriter2 = selector => {
-    const el = document.querySelector(selector)
-    const text = el.innerHTML
+// const typeWriter2 = selector => {
+//     const el = document.querySelector(selector)
+//     const text = el.innerHTML
 
-        ; (function _type(i = 0) {
-            if (i === text.length) return
+//         ; (function _type(i = 0) {
+//             if (i === text.length) return
 
-            el.innerHTML =
-                text.substring(0, i + 1) + '<span aria-hidden="true"></span>'
-            setTimeout(() => _type(i + 1), 100)
-        })()
-}
+//             el.innerHTML =
+//                 text.substring(0, i + 1) + '<span aria-hidden="true"></span>'
+//             setTimeout(() => _type(i + 1), 100)
+//         })()
+// }
 
-setTimeout(typeWriter2(".js-type-writer2"), 1000)
+// setTimeout(typeWriter2(".js-type-writer2"), 1000)
 // FIXME:
+
+var typed = new Typed('.moonOldSay', {
+    strings: ["我最近想去放假...", "", "最近真的太忙了..."],
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true,
+});
 
 
 // 三貓 hover
-// FIXME: 現在是怎樣
-$('.leftCatImg').mouseenter(function () {
-    $('.leftCat img').attr('src', "./imgs/index/7.png");
+$('.leftCat').mouseenter(function () {
+    $('.leftCatImg img').attr('src', "./imgs/index/7.png");
+})
+$('.leftCat').mouseleave(function () {
+    $('.leftCatImg img').attr('src', "./imgs/index/8.png");
+})
+$('.middleCat').mouseenter(function () {
+    $('.middleCatImg img').attr('src', "./imgs/index/9.png");
+})
+$('.middleCat').mouseleave(function () {
+    $('.middleCatImg img').attr('src', "./imgs/index/10.png");
+})
+$('.rightCat').mouseenter(function () {
+    $('.rightCatImg img').attr('src', "./imgs/index/11.png");
+})
+$('.rightCat').mouseleave(function () {
+    $('.rightCatImg img').attr('src', "./imgs/index/12.png");
 })
