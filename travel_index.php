5f773afe9d7b4181e563ddbbc9c0e0e274621a53
elena <jessie84121@gmail.com>
2022-09-21 12:00:11 +0800

    9/21 yu

--- travel_index.php
+++ travel_index.php
@@ -278,7 +278,7 @@ $pageName ='home'; //頁面名稱
 <?php include __DIR__. '/parts/scripts.php'; ?>
 
 
-<script></script>
+<script>
 
         // #move3-yu
         $(window).scroll(function () {
@@ -351,10 +351,6 @@ $pageName ='home'; //頁面名稱
         }
 
         })
-
-
-
-
     </script>
 
 
