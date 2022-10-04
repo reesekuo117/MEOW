7afdf53d327bc38bcd2619efda8d3325fcc034dd
reesekuo117 <toivo.017@gmail.com>
2022-10-04 13:26:19 +0800

    +

--- data/meow.sql
+++ data/meow.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost
--- 產生時間： 2022 年 10 月 04 日 07:15
+-- 產生時間： 2022 年 10 月 04 日 07:25
 -- 伺服器版本： 10.4.21-MariaDB
 -- PHP 版本： 8.0.19
 
@@ -764,7 +764,7 @@ CREATE TABLE `travel_order` (
 -- 傾印資料表的資料 `travel_order`
 --
 
-INSERT INTO `travel_order` (`sid`, `member_id`, `travel_sid`, `state`, `price`, `quantity`, `total`, `name`, `phone`, `address_city`, `address_region`, `address`, `payment`, `payment_state`, `created_at`) VALUES
+INSERT INTO `travel_order` (`sid`, `member_id`, `travel_sid`, `state`, `price`, `quantity`, `totle`, `name`, `phone`, `address_city`, `address_region`, `address`, `payment`, `payment_state`, `created_at`) VALUES
 (1, 16, 3, '訂單完成', 1500, 1, 1500, '橘胖', '0987654321', 5, 26, '18樓', '信用卡', '已付款', '2022-09-16 12:50:00'),
 (2, 16, 6, '訂單完成', 6000, 1, 6000, '橘胖', '0987654321', 5, 26, '18樓', '信用卡', '已付款', '2022-09-17 12:50:00'),
 (3, 16, 9, '訂單完成', 6000, 2, 12000, '橘胖', '0987654321', 5, 26, '18樓', '信用卡', '已付款', '2022-09-18 12:50:00');
