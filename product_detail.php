7b8f8c1e16d578a88be24c1b1e114e38916c8d7b
Reese <toivo.017@gmail.com>
2022-10-07 16:50:46 +0800

    +

--- product_detail.php
+++ product_detail.php
@@ -785,9 +785,8 @@ foreach($plove_rows as $p){
                                                     <div class="img-box">
                                                         <img src="./imgs/product/cards/<?= $p['product_card_img'] ?>.jpg" class="img-fluid" alt="">
                                                         <div class="icon_heart <?= !empty($plove_dict[$p['sid']]) ? 'color' : '' ?>"
-                                                        data-sid="<?= $p["sid"] ?>" onclick="addToFav_P_07(event)"
-                                                        >
-                                                            <svg class="heart_line" width="32" height="32" viewBox="0 0 32 32" fill="none" stroke="#432A0F" xmlns="http://www.w3.org/2000/svg">
+                                                        data-sid="<?= $p["sid"] ?>" onclick="addToFav_P_07(event)">
+                                                            <svg class="heart_line" width="32" height="32" viewBox="0 0 32 32" fill="none" stroke="#fff" xmlns="http://www.w3.org/2000/svg">
                                                                 <path d="M15.2855 9.22197C12.9704 6.90689 9.21692 6.90689 6.90184 9.22197C4.58676 11.537 4.58676 15.2905 6.90184 17.6056L13.2503 23.9532C14.8378 25.5407 17.4116 25.5407 18.9991 23.9532L24.5083 18.444L24.5074 18.4431L25.3449 17.6056C27.66 15.2905 27.66 11.5371 25.3449 9.22197C23.0298 6.90689 19.2763 6.90689 16.9612 9.22197L16.1234 10.0598L15.2855 9.22197Z" stroke-width="2.66667" />
                                                             </svg>
                                                         </div>
